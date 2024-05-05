a725c4d51ceca5df760cd5099f4735e567efc5fc
Ville Saloranta <SRanta.Ville@gmail.com>
2024-05-05 17:49:49 +0300

    Published the code

---
+++ index.js
@@ -0,0 +1,71 @@
+// index.js
+// where your node app starts
+
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+// Providing my changes to the program
+app.get("/api/:date", function(req, res)
+{
+  let date = req.params.date;
+  //Unix type can be recognized as a number
+  if(!isNaN(Number(date)))
+  {
+    return res.json({
+      unix: date,
+      utc: new Date(Number(date)).toUTCString()
+    });
+  }
+
+  // If the date is not in numeric form, it is expected to be of
+  // type string. Checking whether it is valid or not...
+  if(new Date(date).toUTCString() == "Invalid Date")
+  {
+    return res.json({
+      error: "Invalid Date"
+    });
+  }
+
+  // A valid string date will be handled now, as an invalid one
+  // would have triggered a return command.
+  return res.json({
+    unix: new Date(date).getTime(),
+    utc: new Date(date).toUTCString()
+  });
+
+});
+
+app.get("/api", function(req,res)
+{
+  res.json({
+      unix: new Date().getTime(),
+      utc: new Date().toUTCString()
+  });
+});
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
